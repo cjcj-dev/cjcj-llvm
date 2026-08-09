ca778a7cabbb159eb37b0d5043cda30a74799290
Zxilly <zxilly@outlook.com>
2026-08-09 22:59:32 +0800

    semantic(ManagedABIGate): run gate before safepoint rewriting

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -217,6 +217,7 @@
 #include "llvm/Transforms/Scalar/LowerGuardIntrinsic.h"
 #include "llvm/Transforms/Scalar/LowerMatrixIntrinsics.h"
 #include "llvm/Transforms/Scalar/LowerWidenableCondition.h"
+#include "llvm/Transforms/Scalar/ManagedABIGateVerifier.h"
 #include "llvm/Transforms/Scalar/MakeGuardsExplicit.h"
 #include "llvm/Transforms/Scalar/MemCpyOptimizer.h"
 #include "llvm/Transforms/Scalar/MergeICmps.h"
@@ -1255,6 +1256,7 @@ Error PassBuilder::parseModulePass(ModulePassManager &MPM,
         MPM.addPass(createModuleToFunctionPassAdaptor(CJGCInstrRestore()));
       }
       if (CJPipeline && !CangjieLTOPreOpt) {
+        MPM.addPass(ManagedABIGateVerifierPass());
         MPM.addPass(CJSpecificOpt(L.getSpeedupLevel()));
         MPM.addPass(PlaceSafepoints());
         MPM.addPass(CJBarrierOpt());
